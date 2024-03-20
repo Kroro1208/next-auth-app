774b61481b25b7423b37a4577ba73e724dc0f5fb
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-20 17:34:00 +0900

    ログインページ作成した

---
+++ components/common/ErrorToaster.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useToast } from "../ui/use-toast";
+import { useEffect } from "react";
+
+const errorMessages = {
+    OAuthAccountNotLinked: "このメールアドレスは別のログイン方法で既に登録されています。以前に使用したログイン方法をお試しください。"
+};
+
+export const ErrorToaster = () => {
+    const searchParams = useSearchParams();
+    const error = searchParams.get("error");
+    const { toast } = useToast();
+
+    useEffect(() => {
+        if (error) {
+            const errorMessage = errorMessages[error as keyof typeof errorMessages] || "エラーが発生しました";
+            toast({
+                variant: "destructive",
+                title: errorMessage
+            });
+        }
+    }, [error, toast]);
+
+    return null;
+}
